d2ef2b1531627ef8f599ccf24f75ad671c1ab2fb
Edwin Niwarlangga <leonardusedwin.mail@gmail.com>
2024-02-05 14:16:17 +0700

    [iOS] add Target Moya NewList

--- MandiriNewsUsingViper/Constants/Constant.swift
+++ MandiriNewsUsingViper/Constants/Constant.swift
@@ -1,36 +1,16 @@
-////
-////  Constant.swift
-////  MandiriNewsUsingViper
-////
-////  Created by Edwin Niwarlangga on 05/02/24.
-////
-//import Foundation
-//struct Constants {
-//    
-//    struct Urls {
-//        // MARK: Source
-//        static func urlForGetSourceFromCategory(category : String, pageSize : Int, page: Int) -> URL {
-//            return URL(string: "https://newsapi.org/v2/top-headlines/sources?apiKey=\(Constants.API_KEYS)&category=\(category.escaped())")!
-//        }
-//        
-//        
-//        // MARK: ARTICLES
-//        static func urlForGetNewsFromSourceName(sourceKeyword: String, pageSize : Int, page: Int) -> URL {
-////            print("https://newsapi.org/v2/top-headlines?sources=\(sourceKeyword.escaped())&apiKey=\(Constants.API_KEYS)&page=\(page)&pageSize=\(pageSize)")
-//            return URL(string: "https://newsapi.org/v2/top-headlines?sources=\(sourceKeyword.escaped())&apiKey=\(Constants.API_KEYS)&page=\(page)&pageSize=\(pageSize)")!
-//        }
-//        
-//        static func urlForGetNewsFromKeyword(keyword: String, pageSize : Int, page: Int) -> URL {
-//            return URL(string: "https://newsapi.org/v2/top-headlines?apiKey=\(Constants.API_KEYS)&q=\(keyword.escaped())&pageSize=\(pageSize)&page=\(page)")!
-//        }
-//        
-//        static func webViewDefaultValue() -> URL {
-//            
-//            return URL(string: "https://newsapi.org/")!
-//        }
-//        
-//    }
-//    static let API_KEYS = "6447817f8d9c44d2b93bed6580d3f848"
-//    
-//    
-//}
+//
+//  Constant.swift
+//  MandiriNewsUsingViper
+//
+//  Created by Edwin Niwarlangga on 05/02/24.
+//
+import Foundation
+class ConstantURL{
+    
+    static let urlString: String  = "https://newsapi.org/v2"
+}
+
+class ConstantAuth{
+    static let APIKEY : String = "c00b33fac0df44d0b8102227f3cebed6"
+}
+
